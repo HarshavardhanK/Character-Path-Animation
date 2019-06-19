de6c353e143628a7b893b1e5ccde1f2fccbc79fe
Harshavardhan K <harshavardhan816@gmail.com>
2019-06-19 14:49:39 +0530

    initial set up commit

---
+++ Character Path Animation/Parser.swift
@@ -0,0 +1,9 @@
+//
+//  Parser.swift
+//  Character Path Animation
+//
+//  Created by Harshavardhan K on 19/06/19.
+//  Copyright © 2019 Harshavardhan K. All rights reserved.
+//
+
+import Foundation
---
+++ Pods/Target Support Files/Pods-Character Path Animation/Pods-Character Path Animation-umbrella.h
@@ -0,0 +1,16 @@
+#ifdef __OBJC__
+#import <Cocoa/Cocoa.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+
+FOUNDATION_EXPORT double Pods_Character_Path_AnimationVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_Character_Path_AnimationVersionString[];
+
